76600e8b6cfca9087e646674d8d3968f120ca16f
Thalles <thalleslopesm@gmail.com>
2025-04-03 21:25:48 -0300

    Clean up

--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -1,9 +1,5 @@
-//import { Metadata } from 'next'
-
 import { ProjectView } from '@/components/project-view'
 import { loadProjects, loadProject } from '@/utils/load-data'
-
-// import { notFound } from "next/navigation";
 
 export default async function Project(props: Params) {
   const params = await props.params
@@ -19,25 +15,6 @@ type Params = {
     slug: string
   }>
 }
-
-// export async function generateMetadata(props: Params): Promise<Metadata> {
-//   const params = await props.params;
-//   const post = getPostBySlug(params.slug);
-
-//   if (!post) {
-//     return notFound();
-//   }
-
-//   const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`;
-
-//   return {
-//     title,
-//     openGraph: {
-//       title,
-//       images: [post.ogImage.url],
-//     },
-//   };
-// }
 
 export async function generateStaticParams() {
   const projects = loadProjects()
